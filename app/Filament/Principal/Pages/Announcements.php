a5f19568a8d86fb610eef9d251f5aba379cc8d44
Albert Arthur <albert.arthur@binus.edu>
2026-05-28 17:37:41 +0700

    chore(principal): group Announcements with Messages under Communication
    Moved the Announcements page out of the Overview group and into the existing Communication group so both communication tools live together.

--- app/Filament/Principal/Pages/Announcements.php
+++ app/Filament/Principal/Pages/Announcements.php
@@ -18,7 +18,7 @@ class Announcements extends Page implements HasForms, HasActions
     use InteractsWithActions, InteractsWithForms;
 
     protected static ?string $navigationIcon = 'heroicon-o-megaphone';
-    protected static ?string $navigationGroup = 'Overview';
+    protected static ?string $navigationGroup = 'Communication';
     protected static ?string $title = 'Announcements';
     protected static ?int $navigationSort = 1;
     protected static string $view = 'filament.principal.pages.announcements';
